0d1729b8734582dafc4c58e115cd295e6c0ae320
srph <seraphipod@gmail.com>
2016-01-17 07:06:56 +0800

    Add professor view, rate, and search page.

--- common/routes.js
+++ common/routes.js
@@ -2,9 +2,15 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import MainView from './views/main';
 import MainHomeView from './views/main.home';
+import MainSearchView from './views/main.search';
+import MainProfessorView from './views/main.professor';
+import MainRateView from './views/main.rate';
 
 export default (
   <Route path="/" component={MainView}>
     <IndexRoute component={MainHomeView} />
+    <Route path="search" component={MainSearchView} />
+    <Route path="professor/:id" component={MainProfessorView} />
+    <Route path="rate" component={MainRateView} />
   </Route>
 );
---
+++ common/views/main.rate/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainRateView extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default MainRateView;
