aba58bb0a70a554197f5e29215e0620f2ccb0ea6
srph <seraphipod@gmail.com>
2016-01-17 07:18:36 +0800

    Update search page styling.

--- common/views/main.search/index.jsx
+++ common/views/main.search/index.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class MainSearchView extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="search-section">
+          <form>
+            <input type="search" className="searchbar" placeholder="Search" />
+          </form>
+
+          <p className="lead">Find professors, universities.</p>
+        </div>
       </div>
     );
   }
