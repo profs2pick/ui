5f85cae16e34e2f6f15d79e3802efc1d3883eab1
srph <seraphipod@gmail.com>
2016-01-18 06:35:50 +0800

    Init professor page layout.

--- common/views/main.professor/index.jsx
+++ common/views/main.professor/index.jsx
@@ -4,6 +4,19 @@ class MainProfessorView extends React.Component {
   render() {
     return (
       <div>
+        <div className="professor-jumbotron">
+          <div className="inner">
+            <img src="https://avatars3.githubusercontent.com/u/9919?v=3&s=72" className="avatar -large" alt="avatar" />
+            <h1 className="name">Kirby Borromeo</h1>
+            <h4 className="field">Multimedia Arts</h4>
+            <h4 className="university">MMC-Cast</h4>
+
+            <div className="form-actions">
+              <a href="#" className="btn -default">Submit Review</a>
+              <a href="#" className="btn -default">Share</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
