56517332911558fea103e4d99dac31fa6f59a62f
srph <seraphipod@gmail.com>
2016-01-18 07:00:54 +0800

    Add nav search input.

--- common/views/main/index.jsx
+++ common/views/main/index.jsx
@@ -4,17 +4,26 @@ export default class Main extends React.Component {
   render() {
     return (
       <div>
-        <div className="main-nav">
-          <div>
+        <nav className="main-nav">
+          <div className="nav">
             <a href="#" className="link -active">Home</a>
             <a href="#" className="link">Professors</a>
             <a href="#" className="link">Universities</a>
           </div>
 
-          <div>
+          <div className="nav">
+            <div className="searchbar">
+              <form>
+                <div className="input-group">
+                  <span className="addon"><button className="btn -simple">+</button></span>
+                  <input type="search" className="form-control" />
+                </div>
+              </form>
+            </div>
+
             <a href="#" className="btn -default">Submit Rating</a>
           </div>
-        </div>
+        </nav>
 
         {this.props.children}
       </div>
