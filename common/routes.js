dc93c068cf77361961cdb3400947922ae6ac040e
srph <seraphipod@gmail.com>
2016-01-20 02:17:26 +0800

    update route folder name.

--- common/routes.js
+++ common/routes.js
@@ -3,7 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 import MainView from './views/main';
 import MainHomeView from './views/main.home';
 import MainSearchView from './views/main.search';
-import MainProfessorView from './views/main.professor';
+import MainProfessorView from './views/main.professors';
 import MainRateView from './views/main.rate';
 
 export default (
