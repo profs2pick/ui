7663093118344ba7b0d9bc82cd5c08a93e5ce28e
srph <seraphipod@gmail.com>
2016-01-22 01:25:06 +0800

    oops add class to logo markup.

--- common/views/main/index.jsx
+++ common/views/main/index.jsx
@@ -7,7 +7,7 @@ export default class Main extends React.Component {
         <nav className="main-nav">
           <div className="container-fluid">
             <div className="nav">
-              <a href="#"><img src={`${process.env.IMG}/logo.png`} /></a>
+              <a href="#" className="logo"><img src={`${process.env.IMG}/logo.png`} /></a>
             </div>
 
             <div className="nav">
