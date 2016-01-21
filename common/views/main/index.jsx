6ad6211dd4a49e5a3750e8da86b353ab3ad5eea3
srph <seraphipod@gmail.com>
2016-01-22 01:13:25 +0800

    add container-fluid to wrap nav.

--- common/views/main/index.jsx
+++ common/views/main/index.jsx
@@ -5,30 +5,32 @@ export default class Main extends React.Component {
     return (
       <div>
         <nav className="main-nav">
-          <div className="nav">
-            <a href="#"><img src={`${process.env.IMG}/logo.png`} /></a>
-          </div>
-
-          <div className="nav">
-            <a href="#" className="link -active">Home</a>
-            <a href="#" className="link">Professors</a>
-            <a href="#" className="link">Universities</a>
-          </div>
-
-          <div className="nav">
-            <div className="searchbar">
-              <form>
-                <div className="input-group">
-                  <button className="btn"><i className="fa fa-search" /></button>
-                  <input type="search" className="form-control" placeholder="Search..." />
-                </div>
-              </form>
+          <div className="container-fluid">
+            <div className="nav">
+              <a href="#"><img src={`${process.env.IMG}/logo.png`} /></a>
             </div>
 
-            <a href="#" className="btn -default">
-              <i className="fa fa-plus" />
-              Submit Rating
-            </a>
+            <div className="nav">
+              <a href="#" className="link -active">Home</a>
+              <a href="#" className="link">Professors</a>
+              <a href="#" className="link">Universities</a>
+            </div>
+
+            <div className="nav">
+              <div className="searchbar">
+                <form>
+                  <div className="input-group">
+                    <button className="btn"><i className="fa fa-search" /></button>
+                    <input type="search" className="form-control" placeholder="Search..." />
+                  </div>
+                </form>
+              </div>
+
+              <a href="#" className="btn -default">
+                <i className="fa fa-plus" />
+                Submit Rating
+              </a>
+            </div>
           </div>
         </nav>
 
